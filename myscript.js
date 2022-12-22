4881ee550108d73683c63e03f26ec57cf0576426
neuyann <neuyann@gmail.com>
2022-12-22 19:47:21 +0100

    mobile design added

--- myscript.js
+++ myscript.js
@@ -1,8 +1,16 @@
-const button = document.querySelector("button");
+const button = document.querySelector(".divButton");
 const social = document.querySelector(".sharedSocial");
+const image = document.querySelector(".shareButtonImg");
 
 const showDiv = () =>  {
   social.classList.toggle("hidden");
+  if((button.className === "divButton") && (image.className === "shareButtonImg")){
+    button.className = "divButton2";
+    image.className = "shareButtonImg2";
+  } else {
+    button.className = "divButton";
+    image.className = "shareButtonImg";
+  }
 };
 
 button.addEventListener("click", showDiv);
