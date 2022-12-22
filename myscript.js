c0d937cc380c1cb0059c36c4b6fe5c66e86863d4
neuyann <neuyann@gmail.com>
2022-12-22 20:35:57 +0100

    last changes done

--- myscript.js
+++ myscript.js
@@ -1,16 +1,8 @@
-const button = document.querySelector(".divButton");
+const button = document.querySelector("button");
 const social = document.querySelector(".sharedSocial");
-const image = document.querySelector(".shareButtonImg");
 
 const showDiv = () =>  {
   social.classList.toggle("hidden");
-  if((button.className === "divButton") && (image.className === "shareButtonImg")){
-    button.className = "divButton2";
-    image.className = "shareButtonImg2";
-  } else {
-    button.className = "divButton";
-    image.className = "shareButtonImg";
-  }
 };
 
 button.addEventListener("click", showDiv);
