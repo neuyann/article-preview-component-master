945b86ce5686a2023a7433e1861eb38afdba7474
neuyann <neuyann@gmail.com>
2022-12-22 20:53:37 +0100

    margin main added

--- myscript.js
+++ myscript.js
@@ -1,8 +1,20 @@
-const button = document.querySelector("button");
+const button = document.querySelector(".divButton");
 const social = document.querySelector(".sharedSocial");
+const image = document.querySelector(".shareButtonImg");
 
 const showDiv = () =>  {
-  social.classList.toggle("hidden");
+  if (window.matchMedia("(max-width: 900px)").matches) {
+    if((button.className === "divButton") && (image.className === "shareButtonImg")){
+    button.className = "divButton2";
+    image.className = "shareButtonImg2";
+    social.classList.toggle("hidden");
+  } else {
+    button.className = "divButton";
+    image.className = "shareButtonImg";
+    social.classList.toggle("hidden");
+  } 
+ } else {social.classList.toggle("hidden")}
+  
 };
 
 button.addEventListener("click", showDiv);
